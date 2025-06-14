aff2df8f34cbaf4c7371cf3d301b19a043415d8b
kylea <kylea@KYLE>
2025-06-14 10:45:28 -0600

    Se agregaron las carpetas para los recursos y las vistas, y sus respectivos Froms

--- AppBigFood/Views/Cliente/FrmAgregarCliente.cs
+++ AppBigFood/Views/Cliente/FrmAgregarCliente.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AppBigFood
+namespace AppBigFood.Views.Cliente
 {
-    public partial class Form1 : Form
+    public partial class FrmAgregarCliente : Form
     {
-        public Form1()
+        public FrmAgregarCliente()
         {
             InitializeComponent();
         }
